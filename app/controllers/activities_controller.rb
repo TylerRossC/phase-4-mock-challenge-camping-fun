3eba797c9e637bcd5f574535d567ab4d8bcd5c48
tyler <tylercharawell@gmail.com>
2021-08-06 10:02:13 -0700

    commit tree ca9334798d94f6583dca567df98b329c87e83e21

---
+++ app/controllers/activities_controller.rb
@@ -0,0 +1,20 @@
+class ActivitiesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :activity_not_found
+
+    def index
+        activity = Activity.all 
+        render json: activity 
+    end
+
+    def destroy
+        activity = Activity.find(params[:id])
+        activity.destroy
+        head :no_content
+    end
+
+    private 
+
+    def activity_not_found
+        render json: { error: "Activity not found" }, status: :not_found
+    end
+end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :creation_unsuccessful
 
+  private 
+
+  def creation_unsuccessful(e)
+    render json: { errors: e.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
